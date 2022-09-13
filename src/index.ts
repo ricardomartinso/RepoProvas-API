a9a847d58687152a6bcdabfcbaad4cc76eb853e0
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-09-13 12:09:23 -0300

    adjust to heroku deploy

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(router);
 app.use(errorHandler);
 
-const PORT = Number(process.env.PORT) | 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
